77ba0e9fb007ed26d64ed8d0dc0808c307ec942d
MainRyu <00001092512056sp@al.educacao.sp.gov.br>
2024-10-27 16:37:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Anime',
+    'Qual meu anime favorito?',
+    'Jojo'
+)
+
+criaCartao(
+    'Jogo Favorito',
+    'Qual meu jogo favorito?',
+    'KOF'
+)
+
+criaCartao(
+    'Vilão',
+    'Qual o vilão de jojo que mais gosto?',
+    'Kira'
+)
+
+criaCartao(
+    'Comida',
+    'Qual comida mais gosto?',
+    'Lasanha'
+)
